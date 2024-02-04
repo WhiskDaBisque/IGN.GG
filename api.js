9c4676473525b49c8f4fe96a1ff29c760f953c88
sircles <natesungmin@gmail.com>
2024-02-03 19:33:55 -0800

    maybe this one will work

---
+++ api.js
@@ -0,0 +1,21 @@
+const apiKey = 'RGAPI-c3a956de-8e16-447c-ae6c-cc6874646b64'; 
+const baseUrl = 'https://na1.api.riotgames.com/lol/summoner/v4';
+
+async function fetchSummonerByName(summonerName) {
+    const url = `${baseUrl}/summoners/by-name/${summonerName}?api_key=${apiKey}`;
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+      
+      return data;
+    } catch (error) {
+      console.error("Error fetching summoner:", error);
+      throw error;
+    }
+  }
+  
